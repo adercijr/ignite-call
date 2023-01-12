da516bf7b55993a0a97e29610e58e253af7984a4
Aderci Junior <adercidepaula.jr@gmail.com>
2023-01-12 22:52:31 +0000

    setting validation on the homepage form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,19 +1,52 @@
-import { Button, TextInput } from "@ignite-ui/react"
-import { Form } from "./styles"
+import { Button, TextInput, Text } from "@ignite-ui/react"
+import { Form, FormAnnotation } from "./styles"
 import { ArrowRight } from "phosphor-react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const claimUSernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "minimum 3 characters" })
+    .regex(/^([a-z\\\\-]+)$/i, { message: "only letters and hyphens" })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUSernameFormData = z.infer<typeof claimUSernameFormSchema>
 
 export function ClaimUSernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUSernameFormData>({
+    resolver: zodResolver(claimUSernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUSernameFormData) {}
+
   return (
-    <Form as="form">
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="your-user"
-      ></TextInput>
-      <Button size="sm" type="submit">
-        To schedule
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="your-user"
+          {...register("username")}
+        ></TextInput>
+        <Button size="sm" type="submit">
+          To schedule
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : "Enter the intended username."}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
